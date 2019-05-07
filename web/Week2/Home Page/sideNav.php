f4acd459bf1f7084a683ad6cd17cdf1ec8bc4991
Elías <35237918+Monzon-Elias@users.noreply.github.com>
2019-05-07 00:08:06 -0400

    Land Page - php
    A php file containing the side meny bar.

---
+++ web/Week2/Home Page/sideNav.php
@@ -0,0 +1,8 @@
+<?php
+echo '<div id="mySidenav" onmouseover="openNav()" onmouseout="closeNav()">
+        <a href="#home" id="about">Home</a>
+        <a href="#assig1" id="blog">Assig1</a>
+        <a href="#assig2" id="projects">Assig2</a>
+        <a href="#assig3" id="contact">Assig3</a>
+      </div>';
+?>
