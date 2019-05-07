39f4acac1bd6267035e071d266f429032b641c69
Elías Monzón Castro <elidarson@hotmail.com>
2019-05-07 02:04:08 -0400

    Si que la estoy pasando mal

--- web/Week2/Home Page/sideNav.php
+++ web/Week2/Home Page/sideNav.php
@@ -1,6 +1,6 @@
 <?php
-      echo '<a href="#home" id="about">Home</a>
-      <a href="#assig1" id="blog">Assig1</a>
-      <a href="#assig2" id="projects">Assig2</a>
-      <a href="#assig3" id="contact">Assig3</a>';
+      echo '<a href="#home" id="home">Home</a>
+      <a href="#assig1" id="assig1">Assig1</a>
+      <a href="#assig2" id="assig2">Assig2</a>
+      <a href="#assig3" id="assig3">Assig3</a>';
 ?>
