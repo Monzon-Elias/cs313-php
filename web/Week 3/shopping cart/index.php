8506b27545c89bb5e53d3d0bb14820f3f2aac7cf
Elías <35237918+Monzon-Elias@users.noreply.github.com>
2019-05-16 01:29:20 -0400

    index.php

---
+++ web/Week 3/shopping cart/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>PHP Shopping Cart - Week 3</title>
+    </head>
+
+    <body>
+        <!-- 1st item -->
+        <div class="product">
+            <h1>Tulipanes</h1>
+            <a href="add_to_cart.php?id=1"> Add to Cart</a>
+        </div>
+        <!-- 2nd item -->
+        <div class="product">
+            <h1>Claveles</h1>
+            <a href="add_to_cart.php?id=2"> Add to Cart</a>
+        </div>
+        <!-- 3th item -->
+        <div class="product">
+            <h1>Margaritas</h1>
+            <a href="add_to_cart.php?id=3"> Add to Cart</a>
+        </div>
+    </body>
+    
+</html>
