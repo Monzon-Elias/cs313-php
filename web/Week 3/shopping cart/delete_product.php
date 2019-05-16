93d68c97c73b7d10554517115fdd9c239bc0a9c2
Elías <35237918+Monzon-Elias@users.noreply.github.com>
2019-05-16 01:45:14 -0400

    Delete delete_product.php

--- web/Week 3/shopping cart/delete_product.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-array_pop($_SESSION['cart']);
-var_dump($_SESSION['cart']);
-?>
-
-<br><br>
-<a href="index.php">Return to products</a><br>
-<a href="delete_product.php">Delete last product</a>
